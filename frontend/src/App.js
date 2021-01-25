4692f94557d64527a13be7b372301fd5d5b2dabf
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-01-25 14:02:06 +0100

    Starting the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// React Bootstrap
+import { Container } from 'react-bootstrap';
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Pages
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome to the eCommerce</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/product/:id" component={ProductPage} />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
