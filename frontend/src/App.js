3c2a4048e7b857a64615256a1a47ee36d9e5f9a2
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-01-26 15:05:30 +0100

    Place order page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import ShippingPage from './pages/ShippingPage';
 import PaymentPage from './pages/PaymentPage';
+import PlaceOrderPage from './pages/PlaceOrderPage';
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
           <Route path="/cart/:id?" component={CartPage} />
           <Route path="/shipping" component={ShippingPage} />
           <Route path="/payment" component={PaymentPage} />
+          <Route path="/placeorder" component={PlaceOrderPage} />
           <Route path="/" component={HomePage} exact />
         </Container>
       </main>
