c864a20646bf8184a0bdcb274918362249cb7325
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-01-26 10:39:05 +0100

    Auth middleware

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -28,4 +28,25 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { authUser };
+/**
+ * @description Get user profile
+ * @route       GET /api/users/profile
+ * @access      Private
+ */
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+module.exports = { authUser, getUserProfile };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const { protect } = require('./../middleware/auth.middleware');
+
 // Controllers
-const { authUser } = require('./../controllers/user.controller');
+const {
+  authUser,
+  getUserProfile,
+} = require('./../controllers/user.controller');
 
 const router = express.Router();
 
 router.route('/login').post(authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 module.exports = router;
