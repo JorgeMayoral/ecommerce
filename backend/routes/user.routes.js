57dc1a01091a918f520238e55263337b19600c83
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-01-26 10:53:27 +0100

    User registration & password encryption

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -5,11 +5,13 @@ const { protect } = require('./../middleware/auth.middleware');
 // Controllers
 const {
   authUser,
+  registerUser,
   getUserProfile,
 } = require('./../controllers/user.controller');
 
 const router = express.Router();
 
+router.route('/').post(registerUser);
 router.route('/login').post(authUser);
 router.route('/profile').get(protect, getUserProfile);
 
