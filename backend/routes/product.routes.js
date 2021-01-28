1d011dc802293a09012a59100cda0e0e191f23c2
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-01-28 10:01:26 +0100

    Create & update product endpoints

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -7,11 +7,17 @@ const {
   getProducts,
   getProductsById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } = require('./../controllers/product.controller');
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductsById).delete(protect, admin, deleteProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router
+  .route('/:id')
+  .get(getProductsById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 module.exports = router;
