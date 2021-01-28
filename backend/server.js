1cfbb7713f567b1264e7a2e798e5033e07199593
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2021-01-28 11:20:56 +0100

    Image upload config & endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 require('colors');
@@ -8,6 +9,7 @@ const connectDB = require('./config/db');
 const productRoutes = require('./routes/product.routes');
 const userRoutes = require('./routes/user.routes');
 const orderRoutes = require('./routes/order.routes');
+const uploadRoutes = require('./routes/upload.routes');
 
 // Middleware
 const { notFound, errorHandler } = require('./middleware/error.middleware');
@@ -32,10 +34,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID),
 );
+
+app.use('/uploads', express.static(path.join(__dirname, './../uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
